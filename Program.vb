47b4d00bb396b1d80558c38df7a7b2de603252c5
David Hráček <158260270+6mod1@users.noreply.github.com>
2024-06-11 08:37:44 +0200

    Add files via upload

---
+++ Program.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+
+
+Module Program
+    Sub Main(args As String())
+        Dim vstup As String
+        Dim soucet As Integer = 0
+        Dim cislo As Integer
+
+        Do
+            Console.WriteLine("Zadej číslo: ")
+            vstup = Console.ReadLine
+
+            If vstup > 0 Then
+                If vstup < 10 Then
+                    If Integer.TryParse(vstup, cislo) Then
+                        For i = 1 To cislo
+                            soucet = soucet + i
+                        Next
+                        Console.WriteLine($"Souèet od 1 do {cislo} je {soucet}")
+                    Else
+                        Console.WriteLine("Chyba musíte zadat jen celá èísla")
+                    End If
+                Else
+                    Console.WriteLine("Zadali jste moc velkou hodnotu.")
+                End If
+            Else
+                Console.WriteLine("Nelze poèítat se zápornými èísly")
+            End If
+        Loop
+
+
+
+    End Sub
+End Module
